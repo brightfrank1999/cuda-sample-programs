5fa6874999b761eee9b97df43df13460d5b3f8c0
Fangda Cui <cfd86916@gmail.com>
2023-04-17 16:27:46 -0300

    Update

--- vector_add.cu
+++ vector_add.cu
@@ -1,1 +1,54 @@
+#include <stdio.h>
+
+#define N 10000000
+
+__global__ void vector_add(float *out, float *a, float *b, int n) {
+    for(int i = 0; i < n; i++){
+        out[i] = a[i] + b[i];
+    }
+}
+
+int main(){
+    float *a, *b, *out; 
+    float *d_a, *d_b, *d_out;
+
+    // Allocate host memory
+    a   = (float*)malloc(sizeof(float) * N);
+    b   = (float*)malloc(sizeof(float) * N);
+    out = (float*)malloc(sizeof(float) * N);
+
+    // Initialize array
+    for(int i = 0; i < N; i++){
+        a[i] = 1.0f; b[i] = 2.0f;
+    }
+
+    // Allocate device memory
+    cudaMalloc((void**)&d_a, sizeof(float)*N);
+    cudaMalloc((void**)&d_b, sizeof(float)*N);
+    cudaMalloc((void**)&d_out, sizeof(float)*N);
+
+    // Transfer data from host to device memory
+    cudaMemcpy(d_a, a, sizeof(float)*N, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, sizeof(float)*N, cudaMemcpyHostToDevice);
+
+    // Call function
+    vector_add<<<1,1>>>(d_out, d_a, d_b, N);
+
+    // Transfer data from device to host memory 
+    cudaMemcpy(out, d_out, sizeof(float)*N, cudaMemcpyDeviceToHost);
+
+    for(int i = 0; i < 10; i ++){
+         printf("%f\n", out[1]);
+    }
+   
+    // Clean memory
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_out);
+    free(a);
+    free(b);
+    free(out);
+
+    return 0;
+}
 
